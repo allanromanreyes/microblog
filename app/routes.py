018b885938f6d392549a2bd93c86bc62fec7c775
allanromanreyes <allanromanreyes@gmail.com>
2020-06-02 16:49:18 -0700

    Second commit

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
 # Flask mega-tutorial by Miguel Grinberg
 # Routes module
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in LA!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie is so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
